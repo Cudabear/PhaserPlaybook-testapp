89a0b0e2a019f698fec649ee7dd8e088e875524f
Ryan Graef <cudascubby@gmail.com>
2018-04-25 16:32:45 -0500

    first sample json

--- examples/exampleScene.js
+++ examples/exampleScene.js
@@ -10,10 +10,17 @@ ExampleScene = new Phaser.Class({
     preload: function(){
         //TODO: pull this in dynamically
         this.load.plugin('PhaserPlaybook', '../../src/playbook.js');
+
+        //playbooks are just treated as JSON objects
+        this.load.json('example-scene', '../examples/exampleScene.json');
     },
 
     create: function(){
+        //install the playbook on this scene
         this.sys.install('PhaserPlaybook');
+
+        //set the playbook you want to run
+        this.sys.phaserPlaybook.setPlaybook('example-scene');
     },
 
     update: function(){
